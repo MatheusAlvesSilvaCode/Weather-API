1d61d89fa332b137895497825f7f14779fa14d37
MatheusAlvesSilvaCode <matheusalvesdev@outlook.com>
2026-09-23 22:45:49 +0100

    hot fix for security

--- weather-api/src/main/java/weather_api/weather_api/WeatherApiApplication.java
+++ weather-api/src/main/java/weather_api/weather_api/WeatherApiApplication.java
@@ -1,5 +1,4 @@
 package weather_api.weather_api;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class WeatherApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApiApplication.class, args);
 	}
-
 }
--- weather-api/src/main/java/weather_api/weather_api/service/WeatherService.java
+++ weather-api/src/main/java/weather_api/weather_api/service/WeatherService.java
@@ -1,30 +1,62 @@
 package weather_api.weather_api.service;
-import weather_api.weather_api.DTO.WeatherDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
+
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import weather_api.weather_api.DTO.WeatherDTO;
+
+import java.time.Duration;
+
 
 @Service
 public class WeatherService {
 
     private final RestClient restClient;
-
-    private final String apiKey;
     private final ObjectMapper objectMapper;
+    private final StringRedisTemplate redisTemplate;
+    private final String apiKey;
 
+    private static final long CACHE_TTL_HOURS = 60L;
 
-    public WeatherService(@Value("${spring.weather.api.key}") String apiKey) {
-        this.restClient = RestClient.create();
+    public WeatherService(
+            @Value("${spring.weather.api.key}") String apiKey,
+            StringRedisTemplate redisTemplate,
+            ObjectMapper objectMapper
+    ) {
         this.apiKey = apiKey;
-        this.objectMapper = new ObjectMapper();
+        this.restClient = RestClient.create();
+        this.objectMapper = objectMapper;
+        this.redisTemplate = redisTemplate;
     }
 
     public WeatherDTO getWeatherByCity(String city) {
-//        String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
-//                + city + "?unitGroup=metric&key=" + apiKey + "&contentType=json";
+        String cacheKey = "weather:" + city.toLowerCase().trim();
+
+        try {
+            String cachedJson = redisTemplate.opsForValue().get(cacheKey);
+            if (cachedJson != null) {
+                return objectMapper.readValue(cachedJson, WeatherDTO.class);
+            }
+        } catch (Exception e) {
+            System.err.println("Aviso: Falha ao consultar o Redis: " + e.getMessage());
+        }
+
+        WeatherDTO weatherDTO = fetchFromExternalApi(city);
+
+        try {
+            String jsonToCache = objectMapper.writeValueAsString(weatherDTO);
+            redisTemplate.opsForValue().set(cacheKey, jsonToCache, Duration.ofHours(CACHE_TTL_HOURS));
+        } catch (Exception e) {
+            System.err.println("Aviso: Falha ao salvar no Redis: " + e.getMessage());
+        }
+
+        return weatherDTO;
+    }
+
+    private WeatherDTO fetchFromExternalApi(String city) {
         String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
                 + city + "?key=" + apiKey;
 
@@ -32,21 +64,22 @@ public class WeatherService {
                 .uri(url)
                 .retrieve()
                 .body(String.class);
+
         try {
             JsonNode root = objectMapper.readTree(jsonResponse);
             JsonNode current = root.path("currentConditions");
             JsonNode today = root.path("days").get(0);
 
-            return new WeatherDTO( //TODO: ajustar isso, no request está vindo todo estrnaho, morou ?
-                    root.path("address").asString(),
-                    current.path("tempmax").asDouble(),
+            return new WeatherDTO(
+                    root.path("address").asText(),
+                    today.path("tempmax").asDouble(),
                     today.path("tempmin").asDouble(),
-                    today.path("temp").asDouble(),
+                    current.path("temp").asDouble(),
                     current.path("humidity").asDouble(),
-                    current.path("conditions").asString()
+                    current.path("conditions").asText()
             );
         } catch (Exception e) {
-            throw new RuntimeException("Error getting weather by city: " + city, e);
+            throw new RuntimeException("Error to processing : " + city, e);
         }
     }
 }
